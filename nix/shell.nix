4f47e0134014c00cead6d5f3be70581dd7e935b4
Jacob Birkett <jacob@birkett.dev>
2025-10-11 19:03:45 -0700

    nix: shell: add cargo-expand

--- nix/shell.nix
+++ nix/shell.nix
@@ -1,4 +1,4 @@
-{ mkShell, callPackage, rust-bin }:
+{ mkShell, callPackage, rust-bin, cargo-expand }:
 let
   rust-stable = rust-bin.stable.latest.minimal.override {
     extensions = [ "rust-src" "rust-docs" "clippy" ];
@@ -8,5 +8,5 @@ let
   };
 in mkShell {
   strictDeps = true;
-  packages = [ rust-stable rust-nightly-tools ];
+  packages = [ rust-stable rust-nightly-tools cargo-expand ];
 }
