f68e1dc0da4592cef9a4344027764ffec07686af
Jacob Birkett <jacob@birkett.dev>
2025-10-11 23:10:11 -0700

    codegen: generate_openapi: add stub

--- codegen/src/lib.rs
+++ codegen/src/lib.rs
@@ -1,1 +1,10 @@
+use openapiv3::OpenAPI;
+use proc_macro2::TokenStream;
+use quote::quote;
 
+#[derive(Default)]
+pub struct Settings {}
+
+pub fn generate_openapi(_spec: &OpenAPI, _settings: &Settings) -> anyhow::Result<TokenStream> {
+    Ok(quote! {})
+}
