c61e1779ef5c50b2ed72f7cd832735de9b97e20c
Jacob Birkett <jacob@birkett.dev>
2025-10-11 18:40:18 -0700

    nix: formatter: format yaml and json with prettier

--- nix/formatter.nix
+++ nix/formatter.nix
@@ -1,13 +1,16 @@
-{ writeShellApplication, fd, nixfmt, rustfmt }:
+{ writeShellApplication, fd, nixfmt, rustfmt, prettier }:
 writeShellApplication {
   name = "treewide-formatter";
-  runtimeInputs = [ fd nixfmt rustfmt ];
+  runtimeInputs = [ fd nixfmt rustfmt prettier ];
   text = ''
     if [ "''${1:-}" = '--' ]; then
       exec nixfmt
     else
       fd "$@" -H -t file -e nix -X nixfmt '{}'
       fd "$@" -H -t file -e rs -X rustfmt '{}'
+      fd "$@" -H -t file \
+        -e yml -e yaml -e json \
+        -X prettier --log-level error --write '{}'
     fi
   '';
 }
