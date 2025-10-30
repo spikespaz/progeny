6d2fdd4a51483b25117523e0b177ddf8ff02ed41
Jacob Birkett <jacob@birkett.dev>
2025-10-30 13:41:04 -0700

    codegen: type_names: TypeNameTable: impl fn stable_ident_for

--- codegen/src/type_names.rs
+++ codegen/src/type_names.rs
@@ -1,6 +1,7 @@
 use std::collections::HashMap;
 
 use indexmap::IndexSet;
+use quote::format_ident;
 use slotmap::SecondaryMap;
 
 use crate::type_model::TypeId;
@@ -22,5 +23,32 @@ impl TypeNameTable {
 
     pub fn aliases_for(&self, id: TypeId) -> impl Iterator<Item = &syn::Ident> {
         self.aliases.get(id).into_iter().flat_map(|set| set.iter())
+    }
+
+    /// Returns an identifier derived from `base_ident` that this type can safely use.
+    ///
+    /// The returned [`syn::Ident`] is guaranteed not to conflict with any identifiers
+    /// currently owned by other types in the table. Conflicts are resolved by
+    /// a monotonically-increasing suffix.
+    ///
+    /// While solving for a conflict-free identifier, if `id` already owns the current
+    /// candidate, that derivative identifier is returned unchanged.
+    ///
+    /// Otherwise, the first derived identifier that is not owned by any type is returned.
+    ///
+    /// This function does not mutate the table, and callers are responsible for assigning
+    /// a persistent ownership.
+    pub fn stable_ident_for(&self, id: TypeId, base_ident: &syn::Ident) -> syn::Ident {
+        let (mut attempt, mut new_ident) = (0_usize, base_ident.clone());
+        loop {
+            match self.owners.get(&new_ident) {
+                None => break new_ident,
+                Some(&owner_id) if owner_id == id => break new_ident,
+                Some(_) => {
+                    attempt += 1;
+                    new_ident = format_ident!("{base_ident}_{attempt}");
+                }
+            }
+        }
     }
 }
